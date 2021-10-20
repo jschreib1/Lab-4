56d66f4597fb24ef2b62d73acce68438eaaffb35
Jeremy Schreiber <86371980+jschreib1@users.noreply.github.com>
2021-10-20 16:40:11 +0000

    variables

--- pwmBackground.py
+++ pwmBackground.py
@@ -12,18 +12,18 @@ p3 = 13
 GPIO.setup(p1, GPIO.OUT) 
 GPIO.setup(p2, GPIO.OUT) 
 GPIO.setup(p3, GPIO.OUT)
-f = 1000 # frequency (Hz)
+fr = 1000 # frequency (Hz)
 #dc = 50 # duty cycle (%)
-pwm1 = GPIO.PWM(p1,f)
-pwm2 = GPIO.PWM(p2,f)
-pwm3 = GPIO.PWM(p3,f)
+pwm1 = GPIO.PWM(p1,fr)
+pwm2 = GPIO.PWM(p2,fr)
+pwm3 = GPIO.PWM(p3,fr)
 
 pwm1.start(0) # start with LED off
 pwm2.start(0)
 pwm3.start(0)
 while True:
   with open("led-pwm-multiple.txt", 'r') as f:
-    data = float(json.load()) # read duty cycle value from file
+    data = float(json.load(f)) # read duty cycle value from file
     dc = int(data['slider1'])
     led = data['LED']
   if led == 'a': 
